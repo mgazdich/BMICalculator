5c4d8725b14d7547cb7ee18b01c889bcd56d673c
Mike_Gazdich_rMBP <mgazdich@Mike-Gazdich-rMBPs-MacBook-Pro.local>
2013-09-09 19:38:40 -0400

    Initial Commit

---
+++ GazdichAsg1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GazdichAsg1
+//
+//  Created by Mike_Gazdich_rMBP on 9/9/13.
+//  Copyright (c) 2013 Mike Gazdich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GazdichAsg1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GazdichAsg1
+//
+//  Created by Mike_Gazdich_rMBP on 9/9/13.
+//  Copyright (c) 2013 Mike Gazdich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
